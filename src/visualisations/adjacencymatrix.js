bd22895b1242fcd32cac93ea14281746ff485840
Andree34 <52709475+Andree34@users.noreply.github.com>
2021-06-08 20:17:22 +0200

    Add second visualisation prototype

--- src/visualisations/adjacencymatrix.js
+++ src/visualisations/adjacencymatrix.js
@@ -6,20 +6,35 @@ function generateMatrix(edges, nodes) {
   var w = document.getElementById("viscontent").clientWidth;
   var h = document.getElementById("viscontent").clientHeight;
 
+  const squareSize = Math.floor(h / nodes.length) - 1;
+
   var svg = d3
     .select('#' + side)
     .append("svg")
     .attr("height", h)
-    .attr("width", w);
+    .attr("width", w)
+    .style("background", "black");
 
-    svg.append("rect")
+  // svg.append("rect")
+  // .attr("height", 30)
+  // .attr("width", 30)
+  // .attr("fill", "white");
+
+  svg.selectAll("rect")
     .data(edges)
-    .attr("width", (d, i, n) => {
-      console.log(d, i, n);
-      return d.id;
+    .enter()
+    .append("rect")
+    .attr("x", (d) => {
+      return (d.source - 1) * squareSize;
     })
+    .attr("y", (d) => {
+      return (d.target - 1) * squareSize;
+    })
+    .attr("height", squareSize)
+    .attr("width", squareSize)
+    .attr("fill", "white");
 
-  console.log(nodes);
+  console.log(nodes.length);
 }
 
 export default generateMatrix
