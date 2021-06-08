d34eb28ac0eb72886f02b4506c6aa38bc3b960d0
Berkay Kösebay <berkaykosebay@gmail.com>
2021-06-08 23:58:11 +0300

    added the adjacency matrix

--- src/visualisations/adjacencymatrix.js
+++ src/visualisations/adjacencymatrix.js
@@ -1,20 +1,49 @@
 import * as d3 from "d3";
 
-function generateMatrix(edges, nodes){
+function generateMatrix(edges, nodes) {
+
   var side = document.getElementById("testSelect").options[1].value;
   var w = document.getElementById("viscontent").clientWidth;
   var h = document.getElementById("viscontent").clientHeight;
 
+  const squareSize = Math.floor(h / nodes.length) - 1;
+
+  // nodePositions is an object that stores the positions where the nodes should be displayed in the adj matrix
+  var nodePositions = {};
+  for (var i = 0; i < nodes.length; i++) {
+    let position = (nodes[i].employeeID).toString()
+    nodePositions[position] = i;
+  }
+
+
   var svg = d3
-  .select('#' + side)
-  .append("svg")
-  .attr("height", h)
-  .attr("width", w);
-  console.log(svg);
+    .select('#' + side)
+    .append("svg")
+    .attr("height", h)
+    .attr("width", w)
+    .style("background", "black");
 
+  // svg.append("rect")
+  // .attr("height", 30)
+  // .attr("width", 30)
+  // .attr("fill", "white");
 
-  console.log(edges);
-  console.log(nodes);
+  svg.selectAll("rect")
+    .data(edges)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => {
+      return nodePositions[(d.target).toString()] * squareSize;
+    })
+    .attr("y", (d) => {
+      return nodePositions[(d.source).toString()] * squareSize;
+    })
+    .attr("height", squareSize)
+    .attr("width", squareSize)
+    .attr("fill", "white")
+    .style("opacity", 0.15);  // this makes it so that overlayed rectangles can be seen (kind of adds weights to the edges)
+
+  console.log(nodes, edges, nodePositions);
 }
 
 export default generateMatrix
