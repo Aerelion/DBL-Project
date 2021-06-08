d670dfb3cba4fb0baa4d3049fe27620e73b789bb
Andree34 <52709475+Andree34@users.noreply.github.com>
2021-06-08 21:41:56 +0200

    Add bugfixes
    -The matrix is no longer transposed
    -It should be possible to visualise a network of any size now (though the slider may still overlap with the visualisation)

--- src/visualisations/adjacencymatrix.js
+++ src/visualisations/adjacencymatrix.js
@@ -8,6 +8,17 @@ function generateMatrix(edges, nodes) {
 
   const squareSize = Math.floor(h / nodes.length) - 1;
 
+  // nodePositions is an array that stores the positions where the nodes should be displayed in the adj matrix
+  // this array has undefined gaps, but its size will never be larger than the largest node id number
+  // a better approach is by using hash tables, but this should work for the enron dataset
+  // this method is very fast, but doesn't work if this is a large ID number
+  var nodePositions = {};
+  for (var i = 0; i < nodes.length; i++) {
+    let position = (nodes[i].employeeID).toString()
+    nodePositions[position] = i;
+  }
+
+
   var svg = d3
     .select('#' + side)
     .append("svg")
@@ -25,16 +36,16 @@ function generateMatrix(edges, nodes) {
     .enter()
     .append("rect")
     .attr("x", (d) => {
-      return (d.source - 1) * squareSize;
+      return nodePositions[(d.target).toString()] * squareSize;
     })
     .attr("y", (d) => {
-      return (d.target - 1) * squareSize;
+      return nodePositions[(d.source).toString()] * squareSize;
     })
     .attr("height", squareSize)
     .attr("width", squareSize)
     .attr("fill", "white");
 
-  console.log(nodes.length);
+  console.log(nodes, edges, nodePositions);
 }
 
 export default generateMatrix
