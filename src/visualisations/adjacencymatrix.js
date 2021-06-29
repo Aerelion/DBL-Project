6d8d87699f1ffe1b30580d549e153c0ade85d246
Andree34 <52709475+Andree34@users.noreply.github.com>
2021-06-29 16:41:01 +0300

    Upgrade node highlighting in AM

--- src/visualisations/adjacencymatrix.js
+++ src/visualisations/adjacencymatrix.js
@@ -7,10 +7,11 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
   var h = document.getElementById("viscontent").clientHeight;
 
   const textLength = 12;                                                        // change this to the approx max node text length
-  const minOpacity = 0.3;                                                     // opacity of an edge with weight 1
-  const logCoefficient = (1 - minOpacity) / Math.log2(edgeWeights.maxWeight); // coeficient that is used to calculate opacity
   const squareSize = Math.floor(h / (nodes.length + textLength)) - 1;           // the edge side length
-  const textSpace = squareSize * textLength;                                  // approx space allocated for text (important only to center the visualisation when rendering)
+  const strokeSize = squareSize / 4;                                            // the width of the highlight rectangle border
+  const textSpace = squareSize * textLength;                                    // approx space allocated for text (important only to center the visualisation when rendering)
+  const minOpacity = 0.3;                                                       // opacity of an edge with weight 1
+  const logCoefficient = (1 - minOpacity) / Math.log2(edgeWeights.maxWeight);   // coeficient that is used to calculate opacity
 
   // nodePositions is an object that stores the positions where the nodes should be displayed in the adj matrix
   var nodePositions = {};
@@ -68,7 +69,7 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
     .attr("class", (d) => {
       return "id" + d.employeeID;
     })
-    .attr("x", textSpace)
+    .attr("x", textSpace-strokeSize)
     .attr("y", (d) => {
       return (nodePositions[(d.employeeID).toString()] * squareSize) + textSpace + (squareSize * 0.75);
     })
@@ -87,7 +88,7 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
     .attr("class", (d) => {
       return "id" + d.employeeID;
     })
-    .attr("x", textSpace)
+    .attr("x", textSpace-strokeSize)
     .attr("y", (d) => {
       return -((nodePositions[(d.employeeID).toString()] * squareSize) + textSpace + (squareSize * 0.25));
     })
@@ -97,6 +98,45 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
     .style("fill", "white")
     .style("font-size", squareSize);
 
+    var highlightGrid = svg.append("g").attr("id", "highlightGrid");
+    var highlightLeft = highlightGrid.append("g").attr("id", "highlightLeft");  // the text nodes on the left side of the grid
+    var highlightUp = highlightGrid.append("g").attr("id", "highlightUp");      // the text nodes on top of the grid
+  
+    highlightLeft.selectAll("rect")
+      .data(nodes)
+      .enter()
+      .append("rect")
+      .attr("class", (d) => {
+        return "highlightID" + d.employeeID;
+      })
+      .attr("height", squareSize)
+      .attr("width", nodes.length*squareSize)
+      .attr("x", textSpace)
+      .attr("y", (d) =>{
+        return ((nodePositions[(d.employeeID).toString()] * squareSize) + textSpace);
+      })
+      .attr("fill", "none")
+      .attr("stroke", "orange")
+      .attr("stroke-width", strokeSize)
+      .style("visibility", "hidden");
+
+      highlightUp.selectAll("rect")
+      .data(nodes)
+      .enter()
+      .append("rect")
+      .attr("class", (d) => {
+        return "highlightID" + d.employeeID;
+      })
+      .attr("height", nodes.length*squareSize)
+      .attr("width", squareSize)
+      .attr("x", (d) =>{
+        return ((nodePositions[(d.employeeID).toString()] * squareSize) + textSpace);
+      })
+      .attr("y", textSpace)
+      .attr("fill", "none")
+      .attr("stroke", "orange")
+      .attr("stroke-width", strokeSize)
+      .style("visibility", "hidden");
 
   // event handler for clicking on nodes
   text.selectAll("text")
@@ -125,11 +165,11 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
       var index = globalSelection.indexOf(node);
       if (index === -1) {
         svg.selectAll("text.id" + node).style("fill", "red");
-        svg.selectAll("rect.id" + node).style("fill", "orange");
+        svg.selectAll("rect.highlightID" + node).style("visibility", "visible");
         globalSelection.push(node);
       } else {
         svg.selectAll("text.id" + node).style("fill", "white");
-        svg.selectAll("rect.id" + node).style("fill", "white");
+        svg.selectAll("rect.highlightID" + node).style("visibility", "hidden");
         globalSelection.splice(index, 1);
       }
     });
@@ -142,7 +182,7 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
       var index = localSelection.indexOf(node);
       if (index === -1) {
         svg.selectAll("text.id" + node).style("fill", "red");
-        svg.selectAll("rect.id" + node).style("fill", "orange");
+        svg.selectAll("rect.highlightID" + node).style("visibility", "visible");
         localSelection.push(node);
       }
     });
@@ -152,7 +192,7 @@ function generateMatrix(edges, nodes, edgeWeights, globalSelection) {
       var index = globalSelection.indexOf(node);
       if (index === -1) {
         svg.selectAll("text.id" + node).style("fill", "white");
-        svg.selectAll("rect.id" + node).style("fill", "white");
+        svg.selectAll("rect.highlightID" + node).style("visibility", "hidden");
         localSelection.splice(localSelection.indexOf(node), 1);
       }
     });
